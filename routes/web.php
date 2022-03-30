69515f9f42594b84c0479a7c3f8114dfa818e8a8
Luiz Medeiros <luizmedeiros00@gmail.com>
2022-03-30 16:41:37 -0300

    instalando a autenticação padrao do laravel

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::resource('products', ProductController::class);
+
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
