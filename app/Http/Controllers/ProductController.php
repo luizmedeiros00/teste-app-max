dc853c3a9633df236fea37af3283b690705de01e
Luiz Medeiros <luizmedeiros00@gmail.com>
2022-03-31 16:25:53 -0300

    removendo a classe request

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\StoreProductRequest;
 use App\Http\Requests\UpdateProductRequest;
-use Illuminate\Http\Request;
 use App\Models\Product;
 
 class ProductController extends Controller
