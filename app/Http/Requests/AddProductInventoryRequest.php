11cb617d292e91323983621b51e47768a3560d21
Luiz Medeiros <luizmedeiros00@gmail.com>
2022-04-01 13:24:41 -0300

    request de validação para adicionar o produto

---
+++ app/Http/Requests/AddProductInventoryRequest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class AddProductInventoryRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'product_id'    => 'required|numeric|exists:products,id',
+            'amount'        => 'required|numeric|min:1'
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'product_id.required'   => 'O produto é obrigatório',
+            'product_id.numeric'    => 'O produto deve ser do tipo numérico',
+            'product_id.exists'     => 'O produto não existe',
+
+            'amount.required'       => 'A quantidade é obrigatória',
+            'amount.numeric'        => 'A quantidade deve ser do tipo número',
+            'amount.min'            => 'A quantidade deve ser no mínimo :min'
+        ];
+    }
+}
