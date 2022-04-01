886e6edd39c6861f2c440ce191d28208b2d459ca
Luiz Medeiros <luizmedeiros00@gmail.com>
2022-04-01 16:47:11 -0300

    adicionando as rotas adicionar-produto e remover-produto

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\LoginController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,6 +14,13 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+Route::post('login', [LoginController::class, 'login']);
+Route::group(['middleware' => ['jwt.verify']], function () {
+    Route::post('/adicionar-produto', [App\Http\Controllers\Api\MovementController::class, 'add']);
+    Route::post('/baixar-produto', [App\Http\Controllers\Api\MovementController::class, 'remove']);
+});
+
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
