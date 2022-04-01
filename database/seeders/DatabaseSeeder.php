75705d5862caf54560a5a816f66f6472c74c3320
Luiz Medeiros <luizmedeiros00@gmail.com>
2022-04-01 16:45:34 -0300

    adicionando um usuario

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,7 +14,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        \App\Models\User::factory()->create([
+            'email' => 'teste@email.com',
+            'password' => Hash::make('teste123')
+        ]);
         \App\Models\Product::factory(10)->create();
         $this->call(OriginMovementSeeder::class);
         $this->call(TypeMovementSeeder::class);
