dd122a6e91c096268542d840237ace122b4af327
luiz medeiros <luizmedeiros00@gmail.com>
2022-04-02 18:28:31 -0300

    adicionando mais informações

--- resources/views/product/show.blade.php
+++ resources/views/product/show.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <div class="container">
-        <div class="row justify-content-center">
+        <div class="row g-4 justify-content-center">
             <div class="col-md-8">
                 <div class="card">
                     <div class="card-header">Produto</div>
@@ -22,8 +22,50 @@
                                     <input type="text" disabled value="{{ $product->sku }}" class="form-control" </div>
                                 </div>
                             </div>
+                            <div class="col-6">
+                                <div class="form-group">
+                                    <label for="sku">Quantidade em Estoque</label>
+                                    <input type="text" disabled value="{{ $product->inventory->current_amount }}"
+                                        class="form-control" </div>
+                                </div>
+                            </div>
+                            <div class="col-6">
+                                <div class="form-group">
+                                    <label for="sku">Data do cadastro</label>
+                                    <input type="text" disabled value="{{ $product->created_at->format('d/m/Y') }}"
+                                        class="form-control" </div>
+                                </div>
+                            </div>
 
                         </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Movimentações</div>
+
+                    <div class="card-body">
+                        <table class="table">
+                            <thead>
+                                <th>Quantidade Movimentada</th>
+                                <th>Tipo de Movimentação</th>
+                                <th>Via</th>
+                                <th>Data da movimentação</th>
+                            </thead>
+                            <tbody>
+                                @foreach ($product->movements as $movement)
+                                    <tr>
+                                        <td
+                                            style="color: {{ $movement->typeMovement->name == 'Saída' ? 'red' : 'green' }};">
+                                            {{ $movement->amount }}</td>
+                                        <td>{{ $movement->typeMovement->name }}</td>
+                                        <td>{{ $movement->originMovement->name }}</td>
+                                        <td>{{ $movement->created_at->format('d/m/Y') }}</td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
                     </div>
                 </div>
             </div>
