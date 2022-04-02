b65c2c31bd4cb5a64ac33297882486110890d0af
luiz medeiros <luizmedeiros00@gmail.com>
2022-04-02 18:59:38 -0300

    implementando o partials.alert

--- resources/views/product/add.blade.php
+++ resources/views/product/add.blade.php
@@ -2,26 +2,7 @@
 
 @section('content')
     <div class="container">
-        @if (session('success'))
-            <div class="row justify-content-center">
-                <div class="col-md-5">
-                    <div class="alert alert-success alert-dismissible fade show">
-                        {{ session('success') }}
-                        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-                    </div>
-                </div>
-            </div>
-        @endif
-        @if (session('error'))
-            <div class="row justify-content-center">
-                <div class="col-md-5">
-                    <div class="alert alert-danger alert-dismissible fade show">
-                        {{ session('error') }}
-                        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-                    </div>
-                </div>
-            </div>
-        @endif
+        @include('partials.alerts')
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card">
