3dce1846712e9a71097154afda9f73680d17b105
luiz medeiros <luizmedeiros00@gmail.com>
2022-04-02 15:05:01 -0300

    setando o nome teste no usuario

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,8 +15,9 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         \App\Models\User::factory()->create([
-            'email' => 'teste@email.com',
-            'password' => Hash::make('teste123')
+            'name'      => 'Teste',
+            'email'     => 'teste@email.com',
+            'password'  => Hash::make('teste123')
         ]);
         \App\Models\Product::factory(10)->create();
         $this->call(OriginMovementSeeder::class);
